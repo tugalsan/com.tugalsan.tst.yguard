a1e72814043bb8ed48a70d9cea2540afe778272b
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-05-10 18:28:53 +0300

    Initial commit

---
+++ src/main/java/com/tugalsan/tst/yguard/Main.java
@@ -0,0 +1,9 @@
+package com.tugalsan.tst.yguard;
+
+public class Main {
+
+    public static void main(String... s) {
+		System.out.println("Hello YGuard!");
+    }
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,2 @@
+module com.tugalsan.tst.yguard {
+}
